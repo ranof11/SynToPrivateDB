edff9ea0aaeed9acb30f86ed7d9242847bf58dd9
RajeshTriadiNoftarizal <rajeshtriadi14@gmail.com>
2024-10-29 23:16:47 +0700

    feat: refactor CoreDataManager to support any entity type

--- SynToPrivateDB/Manager/CoreDataManager.swift
+++ SynToPrivateDB/Manager/CoreDataManager.swift
@@ -13,5 +13,58 @@ class CoreDataManager {
     
     let persistentContainer: NSPersistentCloudKitContainer
     
+    var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
     
+    private init() {
+        persistentContainer = NSPersistentCloudKitContainer(name: "SynToPrivateDB")
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        
+        context.automaticallyMergesChangesFromParent = true
+        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+    }
+    
+    // MARK: - Fetch Method for Any Entity
+    func fetchEntity<T: NSManagedObject>(_ entity: T.Type, sortDescriptor: NSSortDescriptor? = nil) -> [T] {
+        let request = T.fetchRequest()
+        request.sortDescriptors = sortDescriptor != nil ? [sortDescriptor!] : []
+        
+        do {
+            return try context.fetch(request) as? [T] ?? []
+        } catch {
+            print("Failed to fetch entities: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    // MARK: - Add Method with Configurable Properties
+    func addEntity<T: NSManagedObject>(_ entity: T.Type, configure: (T) -> Void) {
+        let newEntity = T(context: context)
+        configure(newEntity)
+        
+        saveContext()
+    }
+    
+    // MARK: - Delete Method for Any Entity
+    func deleteEntity<T: NSManagedObject>(_ entity: T.Type, at offsets: IndexSet) {
+        let itemToDelete = fetchEntity(entity)
+        offsets.map { itemToDelete[$0] }.forEach(context.delete)
+        
+        saveContext()
+    }
+    
+    // MARK: - Save Context
+    private func saveContext() {
+        do {
+            try context.save()
+        } catch {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
 }
