1f13c8ce05f6f424028d80380f0737c7bc3037ca
RajeshTriadiNoftarizal <rajeshtriadi14@gmail.com>
2024-11-17 17:38:18 +0700

    chore: remove Item entity from Core Data model

---
+++ SynToPrivateDB/Manager/Protocols/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  SynToPrivateDB
+//
+//  Created by Rajesh Triadi Noftarizal on 02/11/24.
+//
+
+import CoreData
+
+protocol DataManager {
+    func addEntity<T: NSManagedObject>(_ entity: T.Type, configure: (T) -> Void)
+    func deleteEntity<T: NSManagedObject>(_ entity: T.Type, at offsets: IndexSet)
+    func updateEntity<T: NSManagedObject>(_ entity: T.Type, withIdentifier identifier: NSManagedObjectID, configure: (T) -> Void)
+    func fetchEntity<T: NSManagedObject>(_ entity: T.Type) -> [T]
+    func setDelegate(_ delegate: CoreDataManagerDelegate)
+}
